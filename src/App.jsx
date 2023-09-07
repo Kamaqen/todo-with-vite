dfb4a3a25811615e9650718fb454bc1ee0b88ce0
“Kamaqen” <jmcapcha@gmail.comgit config --global core.autocrlf inputgit config --global core.safecrlf truegit config --global core.editor code>
2023-09-07 11:39:17 -0500

    ADD: state variables with functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
+import { useState } from "react";
 import "./style.css";
 
-function App() {
+const App = () => {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleChange(event) {
+    setNewItem(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    setTodos([
+      ...todos,
+      { id: crypto.randomUUID(), title: newItem, completed: false },
+    ]);
+    setNewItem("");
+  }
+  function toggleTodo(id, checked) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: checked };
+        }
+        return todo;
+      });
+    });
+  }
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input
+            type="text"
+            id="item"
+            onChange={handleChange}
+            value={newItem}
+          />
         </div>
         <button className="btn">Add</button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.length === 0 && "Emptier than your soul."}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
-}
-
+};
 export default App;
